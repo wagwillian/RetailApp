3a7fd49d28c6a0f0ef807fed8b2c1eb96b72e748
wagwillian <60401249+wagwillian@users.noreply.github.com>
2024-02-01 02:52:22 -0300

    Added a User talbe for internal usage

---
+++ RetailAppDb/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] NVARCHAR(450) NOT NULL PRIMARY KEY,
+	[FirstName] NVARCHAR(50) NOT NULL,
+	[LastName] NVARCHAR(50) NOT NULL,
+	[EmailAddress] NVARCHAR(256) NOT NULL,
+	[CreatedDate] DateTime2 NOT NULL Default getutcdate()
+)
